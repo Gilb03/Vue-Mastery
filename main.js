7e508a28c6c8fb264fb2b14158cd434890ba4aee
Gilbert King <gilbertking@Gilberts-MacBook-Pro.local>
2019-07-24 18:44:36 -0500

    vue mastery course exec.1

---
+++ main.js
@@ -0,0 +1,54 @@
+var app = new Vue ({
+    el : '#app',
+    data: {
+        brand: 'Vue Mastery',
+        product: 'Socks',
+        selectedVariant: 0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: "green",
+            variantImage: './assets/green-socks.jpg',
+            variantQuantity: 10
+          },
+          {
+            variantId: 2235,
+            variantColor: "blue",
+            variantImage: './assets/blue-socks.jpg',
+            variantQuantity: 0
+
+          }
+        ],
+        cart: 0
+    },
+    methods: {
+
+      addToCart(){
+        this.cart += 1
+      },
+
+      updateProduct(index){
+        this.selectedVariant = index
+        console.log(index)
+      },
+      removeFromCart(product){
+        this.cart -= 1
+      }
+
+
+    },
+    computed: {
+      title(){
+        return this.brand + '' + this.product
+      },
+
+      image(){
+        return this.variants[this.selectedVariant].variantImage
+      },
+
+      inStock(){
+        return this.variants[this.selectedVariant].variantQuantity
+      }
+    }
+})
